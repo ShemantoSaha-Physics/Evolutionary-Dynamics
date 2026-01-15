d76c2ba8a6ae9f2086bb99b730cc034545637beb
ShemantoSaha-Physics <mr.shemantosaha@gmail.com>
2026-01-15 22:21:04 +0600

    Update Final lattice Snapshots.py

--- Final lattice Snapshots.py
+++ Final lattice Snapshots.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 from collections import Counter
 from multiprocessing import Pool
 
-# Constants
 R0, S0, T0, P0 = 5.0, 1.0, 3.0, 0.0
 R1, S1, T1, P1 = 3.0, 0.0, 5.0, 1.0
 theta = 2.0
